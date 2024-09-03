12012088d21466e68d53f0c52a6fbb6d973d039a
Darsh-Joshi-10 <darsh10joshi@gmail.com>
2024-09-03 12:17:25 +0530

    trained the model again with higher accuracy

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ history = model.fit(
     steps_per_epoch=train_generator.samples // train_generator.batch_size,
     validation_data=val_generator,
     validation_steps=val_generator.samples // val_generator.batch_size,
-    epochs=250
+    epochs=100
 )
 
 # Evaluate the model
